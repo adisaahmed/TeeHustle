86f1988a6e60c6d819e91a51bdc593288444bb88
Amco <adisa.ahmed881@gmail.com>
2016-06-02 23:21:09 +0100

    Fixing

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
-from forms import LoginForm
+from forms import JobForm
 from services import settings
 from models import Job
 
@@ -8,38 +8,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	apps = Index.query.all()
 	form = JobForm()
 	return render_template('index.html', **locals())
-
-@app.route('/apps/create/', methods=['POST', 'GET'])
-def create_index_view():
-	form = CreateLoginForm()
-	if request.method == 'POST' and form.validate_on_submit():
-		obj = settings.create_app(**form.data)
-		flash('Application has been added successfully')
-		if obj:
-			return redirect(url_for(''))
-	return render_template('index.html', **locals())
-
-@app.route('/apps/update/<int:id>', methods=['POST', 'GET'])
-def update_index_view(id):
-	application = App.query.get(id)
-	form = JobForm(obj=application)
-	if request.method == 'POST' and form.validate_on_submit():
-		obj = settings.update_app(application.id, **form.data)
-		print obj.name
-		flash('Application has been updated successfully')
-		if obj:
-			return redirect(url_for(''))
-	return render_template('index.html', **locals())
-
-@app.route('/index/delete/<int:id>', methods=['POST'])
-def delete_index_view(id):
-	application = App.query.get(id)
-	settings.delete_index(application.id)	
-	flash('Deleted')
-	return redirect(url_for(''))
 
 
 if __name__ == '__main__':
