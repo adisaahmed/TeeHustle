0b5761b432ed3cddd9e6548f159f292f92b3bca7
Amco <adisa.ahmed881@gmail.com>
2016-06-03 23:03:02 +0100

    new fix

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, request, redirect
 from forms import JobForm
 from services import settings
-from models import Job
+# from models import Job
+# from flask.ext.sqlalchemy import SQLAlchemy
+
+# app = Flask(__name__)
+# app.config.from_object(os.environ['APP_SETTINGS'])
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
 
 
 app = Flask(__name__)
--- config.py
+++ config.py
@@ -1,8 +1,15 @@
-WTF_CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'
-
-import os
+# import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+
+
+
+# class Config(object):
+#     DEBUG = False
+#     TESTING = False
+#     CSRF_ENABLED = True
+#     SECRET_KEY = 'this-really-needs-to-be-changed'
+#     SQLALCHEMY_DATABASE_URI = os.environ['jobform']
